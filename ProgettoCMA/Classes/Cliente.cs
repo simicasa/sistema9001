61c2fa0d0b09a084d1e9ef932c03c64d9bd5a7d9
Simone Casaburi <simicasa@hotmail.it>
2017-01-02 10:51:43 +0100

    update igniore

--- ProgettoCMA/Classes/Cliente.cs
+++ ProgettoCMA/Classes/Cliente.cs
@@ -8,20 +8,6 @@ namespace ProgettoCMA
 {
     public partial class Cliente : Azienda
     {
-        /*
-        public Cliente(int ID) : this()
-        {
-            this.ID = ID;
-            this.CodFisc = "";
-            this.Creazione = "";
-            this.Indirizzo = new Indirizzo();
-            this.Mail = "";
-            this.Note = "";
-            this.Piva = "";
-            this.Ragione = "";
-            this.Telefono = "";
-        }
-        */
         public Cliente(int ID, String CF, Indirizzo indirizzo, String email, String note, String PIVA, String ragione, String telefono) : this()
         {
             this.ID = ID;
